d175a26f45b546eab836c010491cff3b8cfd871f
Karl Wootten <karl.wootten@obsequiosoftware.com>
2019-02-27 14:58:49 +0000

    Resolved duplciate record status

--- src/Dfc.CourseDirectory.Models/Enums/Enums.cs
+++ src/Dfc.CourseDirectory.Models/Enums/Enums.cs
@@ -5,23 +5,6 @@ using System.Text;
 
 namespace Dfc.CourseDirectory.Models.Enums
 {
-    //public enum RecordStatus
-    //{
-    //    [Description("Undefined")]
-    //    Undefined = 0,
-    //    [Description("Pending")]
-    //    Pending = 1,
-    //    [Description("Live")]
-    //    Live = 2,
-    //    [Description("Archived")]
-    //    Archived = 3,
-    //    [Description("Deleted")]
-    //    Deleted = 4,
-    //    [Description("Ready To Go Live")]
-    //    ReadyToGoLive = 5
-    //}
-
-    // BitMask
     //[Flags]
     public enum RecordStatus
     {
