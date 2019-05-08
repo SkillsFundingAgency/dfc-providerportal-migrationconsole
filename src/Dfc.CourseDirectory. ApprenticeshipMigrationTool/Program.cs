0bc2c335f15fa915220a848a5984a37553db8d6c
Anatoli Malev <makcom@hotmail.co.uk>
2019-05-08 18:02:40 +0100

    Dfc.CourseDirectory. ApprenticeshipMigrationTool initial commit

---
+++ src/Dfc.CourseDirectory. ApprenticeshipMigrationTool/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Dfc.CourseDirectory._ApprenticeshipMigrationTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
